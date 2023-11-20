304c058283a995f53a0bab9ab4f8075ab21daee9
darshit274 <darshitpatel518@gmail.com>
2023-11-20 10:23:01 +0530

    change in the post index file

--- src/components/pages/Posts/index.js
+++ src/components/pages/Posts/index.js
@@ -1,38 +1,40 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Posts = () => {
-    const [posts, setPosts] = useState([]);
-    useEffect(() => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         let url = process.env.REACT_APP_API_URL + "/posts";
         // let url = "http://localhost/headless/server/wp-json/wp/v2/posts";
         // console.log("API URL:", url);
-        axios.get(url).then((res) => {
-        // console.log(res.data);
-        setPosts(res.data);
-       },[]);
-        
-    });
-    
+        const response = await axios.get(url);
+        // console.log(response.data);
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-    return (
+    fetchData(); // Call the fetchData function
+
+  }, []); // Empty dependency array to run the effect only once
+
+  return (
     <>
-     {
-        posts.map((post) => { 
-            // const html = post.content.rendered;
-            return (
-                <div key={post._id}>
-                    <h2  key={post.id}>{post.title.rendered}</h2>
-                    
-                    <p dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
-                </div>
-            )
-        })
-     }   
+      {posts.map((post) => {
+        // const html = post.content.rendered;
+        return (
+          <div key={post.id}>
+            <h2 key={post.id}>{post.title.rendered}</h2>
+            <p dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
+          </div>
+        );
+      })}
     </>
-    );
-
-      
+  );
 };
 
 export default Posts;
