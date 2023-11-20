4c02d75ff0c1af575ada706552ed5e48e2bfdcfb
darshit274 <darshitpatel518@gmail.com>
2023-11-20 12:10:13 +0530

    tailwind configure

--- src/components/pages/Posts/index.js
+++ src/components/pages/Posts/index.js
@@ -24,15 +24,24 @@ const Posts = () => {
 
   return (
     <>
+    <div className="flex flex-wrap m-3"> 
       {posts.map((post) => {
-        // const html = post.content.rendered;
-        return (
-          <div key={post.id}>
-            <h2 key={post.id}>{post.title.rendered}</h2>
+          // const html = post.content.rendered;
+          return (
+      <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+        <div className="bg-cover h-48" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900)' }}></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 cclassNamelass="mb-4 text-2xl" key={post.id}>{post.title.rendered}</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
             <p dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
+            </div>
           </div>
-        );
-      })}
+        </div>
+      </div>
+      );
+    })}
+  </div>
     </>
   );
 };
